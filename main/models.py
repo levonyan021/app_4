eec68cf596a4ab59f6705b4a8b4e88b3f16a2196
levonyan021 <erik_levonyan@yahoo.com>
2023-01-09 21:11:17 +0400

    Models, Admin, DB, gallery

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class GalleryModel(models.Model):
+    desc = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='imgs/')
+
+    def __str__(self):
+        return self.desc
+
--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse, Http404
-
+from .models import *
 
 def index(request):
-    return render(request, 'main/index.html')
+    element = GalleryModel.objects.order_by('-desc')
+    context = {'element':element}
+    return render(request, 'main/index.html',context=context)
